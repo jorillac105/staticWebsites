280bd3c54825ffb29aaf0abd8e797530c62b991c
Julian Orillac <jorilla1@jhu.edu>
2017-11-29 13:33:31 -0500

    added new font and color scheme

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 $(document).ready(() => {
-    $('.clickable').on('click', () => {
-        $('.overlay').show();
-        $('#whereEducation').slideToggle();
+    let $curWindow;
+
+    $('#edu').on('click', () => {
+        $('.overlay').fadeToggle('slow');
+        $('#whereEducation').slideToggle('fast');
+        $curWindow = $('#whereEducation');
+    });
+
+    $('.overlay').on('click', () => {
+
+        $curWindow.slideToggle('fast');
+        $('.overlay').fadeToggle('slow');
+    })
+
+    $('#exp').on('click', () => {
+        $('.overlay').fadeToggle('slow');
+        $curWindow = $('#whereExp');
+        $curWindow.slideToggle('fast');
     });
 
 });
