d5dc9dab7cebde5ecb2a43ae59095b5458f4a7c1
Julian Orillac <jorilla1@jhu.edu>
2017-11-29 20:39:42 -0500

    made links scroll instead of go away

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+
+    //toggling windows from grid
     let $curWindow;
 
     $('#edu').on('click', () => {
@@ -17,4 +19,30 @@ $(document).ready(() => {
         $curWindow = $('#whereExp');
         $curWindow.slideToggle('fast');
     });
+
+    //scrolling to items from navbar
+    $('.sender').on('click', () => {
+        $('#aboutAnchor').goTo();
+    });
+
+    $('#homeButton').on('click', () => {
+        $('#homeAnchor').goTo();
+    });
+
+    $('#aboutButton').on('click', () => {
+        $('#aboutAnchor').goTo();
+    });
+
+    $('#gridButton').on('click', () => {
+        $('#gridAnchor').goTo();
+    });
 });
+
+(function($) {
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top + 'px'
+        }, 'fast');
+        return this;
+    }
+})(jQuery);
